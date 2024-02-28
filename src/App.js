955a10cc9b08c3d1e9bcf4a1a0b5d11ed17b8087
Ozgur Ozhan <ozgur2096@gmail.com>
2024-02-28 17:56:26 +0100

    react-router-dom added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import CharacterCreation from './CharacterCreation';
 import Dice from './Dice';
 
 function App() {
   return (
-    <div className='App'>
-      <CharacterCreation />
-      {/* <Dice /> */}
-    </div>
+    <Router className='App'>
+      <Routes>
+        <Route path='/' element={<CharacterCreation />} />
+        <Route path='/dice' element={<Dice />} />
+      </Routes>
+    </Router>
   );
 }
 
